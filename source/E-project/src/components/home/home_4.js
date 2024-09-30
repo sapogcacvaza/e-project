7d4369b34ede8d1f03bec8dbddb5944b6d05cd25
sapogcacvaza <vuthanhlong909@gmail.com>
2024-09-30 16:48:08 +0700

    home4 1

--- source/E-project/src/components/home/home_4.js
+++ source/E-project/src/components/home/home_4.js
@@ -7,12 +7,16 @@ export default function Home_4() { //Banner Section
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-6">
-                            <Image src="img/banner/two1.webp" />
-                            <a className="btn btn-view">Buy Now</a>
+                            <a className="two-links opacity" href="/shop" style={{ position: 'relative' }}>
+                                <a className="btn btn-view" style={{ position: 'absolute', color: '#fff', left: '30%', top: '40%' }}>Buy Now</a>
+                                <Image src="img/banner/two1.webp" />
+                            </a>
                         </div>
                         <div className="col-lg-6">
-                            <Image src="img/banner/two2.webp" />
-                            <a className="btn btn-view">Buy Now</a>
+                            <a className="two-links opacity" href="/shop" style={{ position: 'relative' }}>
+                                <a className="btn btn-view" style={{ position: 'absolute', color: '#fff', left: '30%', top: '40%' }}>Buy Now</a>
+                                <Image src="img/banner/two2.webp" />
+                            </a>
                         </div>
                     </div>
                 </div>
