bdebfab8a3e336e2fafe5294776f54658e947380
nguyenvandang113 <nguyenvaddan113@gmail.com>
2024-10-02 10:43:41 +0700

    dádfsfdsfsdaf

--- source/E-project/src/components/navbar.js
+++ source/E-project/src/components/navbar.js
@@ -1,7 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import { useState, useRef, useEffect } from "react";
+
+
+
+
 
 export default function Navbar() {
+    // State để quản lý việc hiển thị form tìm kiếm
+    const [showSearch, setShowSearch] = useState(false);
+    // Tạo ref để tham chiếu tới form và icon tìm kiếm
+    const searchFormRef = useRef(null);
+    const searchIconRef = useRef(null);
+    // Hàm toggle để ẩn/hiển form tìm kiếm
+    const toggleSearchForm = () => {
+        setShowSearch(!showSearch); // Đảo ngược trạng thái hiển thị của form
+    };
+    // Hàm để xử lý khi click ra ngoài form tìm kiếm
+    const handleClickOutside = (event) => {
+        if (
+            searchFormRef.current &&
+            searchIconRef.current &&
+            !searchFormRef.current.contains(event.target) &&
+            !searchIconRef.current.contains(event.target)
+        ) {
+            setShowSearch(false); // Ẩn form khi click ra ngoài
+        }
+    };
+
+    // Sử dụng useEffect để thêm và xóa sự kiện click
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className="container-fluid fixed-top">
             <div className="container px-0">
@@ -29,6 +63,13 @@ export default function Navbar() {
                             <Link to={'/about'} className="nav-item nav-link text-white ">Helping Communities</Link>
                         </div>
                         <div className="navbar-nav d-flex">
+                            <Link to={'/'} className="nav-item nav-link text-white" style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
+                                <form id="search-form" ref={searchFormRef} style={{ position: 'absolute', right: '50px', display: showSearch ? 'block' : 'none' }}>
+                                    <input placeholder="Search" className="input" name="text" type="text" />
+                                </form>
+
+                                <i id="search-icon" onClick={toggleSearchForm} ref={searchIconRef} className="bi bi-search"></i>
+                            </Link>
                             <Link to={'/'} className="nav-item nav-link text-white "><i class="bi bi-person"></i></Link>
                             <Link to={'/'} className="nav-item nav-link text-white "><i class="bi bi-cart"></i></Link>
                         </div>
